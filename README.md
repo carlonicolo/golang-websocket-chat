7167bbb1838c1d279603b5a597b6aba7d7f86002
Carlo <carlo.nicolo@hotmail.com>
2022-07-09 01:00:06 +0200

    Project commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# golang-websocket-chat
+# Golang websocket chat
+
+Simple chat in golang using websockets.
+
+I have used PyCharm IDE for this project then you can import the project and then(if you are using Windows) in the terminal execute:   
+```go run .\cmd\web\.```
+
+![Execution-image](golangChat.png)
+
---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"github.com/carlonicolo/golangChat/internal/handlers"
+	"net/http"
+)
+
+// routes defines the application routes
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
